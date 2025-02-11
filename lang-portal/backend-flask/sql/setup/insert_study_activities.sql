eb557682686ac6d9dad8a3de17c416349f7fd7fa
William Chien <william.chien@mail.mcgill.ca>
2025-02-11 01:00:08 -0500

    backend specs.

--- lang-portal/backend-flask/sql/setup/insert_study_activities.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO study_activities (name, url, preview_url) VALUES 
-('Typing Tutor', 'http://localhost:8080', '/assets/study_activities/typing_tutor.png');
--- lang-portal/backend-flask/tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task
-from lib.db import db
-
-@task
-def init_db(c):
-  from flask import Flask
-  app = Flask(__name__)
-  db.init(app)
-  print("Database initialized successfully.")
